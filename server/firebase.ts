689d080e6c3d40cbe0c9d4f42360456b52ad27b5
Chaitanya Shinde <9chaitanyashinde@gmail.com>
2025-07-27 01:09:40 +0530

    Initial Commit

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,5 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json"; // Correct path
+// import serviceAccount from "./serviceAccountKey.json"; 
+
+// Ensure the environment variable is set
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  throw new Error('The FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set.');
+}
+
+// Parse the JSON string from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
